3b304de0e9b3842f1c5161ef1c37c7581e9e3fcd
billypon <billypon@gmail.com>
2016-12-20 18:04:35 +0800

    update link-auto-jump: support fxpan.com

--- link-auto-jump.js
+++ link-auto-jump.js
@@ -2,10 +2,11 @@
 // @name        链接自动跳转
 // @author      billypon
 // @description 访问分享链接时自动跳转至下载页面或验证页面
-// @version     1.0.0
+// @version     1.1.0
 // @namespace   http://www.canaansky.com/
 // @match       http://158pan.cn/file-*.html
 // @match       http://66yp.cc/file-*.html
+// @match       http://www.fxpan.com/downhtml/*.php
 // @run-at      document-idle
 // @grant       none
 // ==/UserScript==
@@ -53,13 +54,20 @@ function jump(x, y, z) {
 	}
 }
 
-var hostname = location.hostname;
-console.info("hostname:", hostname);
-switch (hostname) {
+var domain = location.hostname.match(/\w+\.\w+$/)[0];
+console.info("domain:", domain);
+switch (domain) {
 	case "158pan.cn":
 		jump();
 		break;
 	case "66yp.cc":
 		jump(null);
+		brea;
+	case "fxpan.com":
+		var a = document.querySelector(".d3 a");
+		if (a) {
+			console.info("link:", a);
+			location = a.href;
+		}
 		break;
 }
