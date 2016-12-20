a0d98ce58ef47f1dfbda6263366e1fc6ef2f32b5
billypon <billypon@gmail.com>
2016-12-20 20:13:00 +0800

    update link-auto-jump: support yunfile.com

--- link-auto-jump.js
+++ link-auto-jump.js
@@ -2,11 +2,13 @@
 // @name        链接自动跳转
 // @author      billypon
 // @description 访问分享链接时自动跳转至下载页面或验证页面
-// @version     1.1.0
+// @version     1.2.0
 // @namespace   http://www.canaansky.com/
 // @match       http://158pan.cn/file-*.html
 // @match       http://66yp.cc/file-*.html
 // @match       http://www.fxpan.com/downhtml/*.php
+// @match       http://*.dfpan.com/fs/*
+// @match       http://*.dfpan.com/file/*
 // @run-at      document-idle
 // @grant       none
 // ==/UserScript==
@@ -54,6 +56,10 @@ function jump(x, y, z) {
 	}
 }
 
+function startsWith(string, prefix) {
+	return string.slice(0, prefix.length) == prefix;
+}
+
 var domain = location.hostname.match(/\w+\.\w+$/)[0];
 console.info("domain:", domain);
 switch (domain) {
@@ -68,6 +74,70 @@ switch (domain) {
 		if (a) {
 			console.info("link:", a);
 			location = a.href;
+		}
+		break;
+	case "dfpan.com":
+		if (startsWith(path, "/file/down/")) {
+			setTimeout(function () {
+				var button = document.querySelector("#downbtn a");
+				console.info("button:", button);
+				if (button)
+					button.click();
+			}, 1000);
+		}
+		else if (startsWith(path, "/fs/") || startsWith(path, "/file/")) {
+			// remove dialog
+			var dialog = document.querySelectorAll("#skyblue_dlg2, #login_registBox2");
+			console.info("dialog:", dialog);
+			if (dialog.length) {
+				for (var i = 0; i < dialog.length; i++) {
+					dialog[i].remove();
+				}
+			}
+
+			// show verify code
+			var button = document.querySelector("#inputDownWait .slow_button");
+			console.info("button wait:", button);
+			if (!button)
+				return;
+			button.click();
+
+			// handle button click
+			var code = document.querySelector("#vcode");
+			console.info("code:", code);
+			button = document.querySelector("#vcode_th .slow_button");
+			console.info("button submit:", button);
+			var a = document.querySelector("#premium_link");
+			console.info("link:", a);
+			if (!(code && button && link))
+				return;
+			button.onclick = "";
+			button.addEventListener("click", function () {
+				// create link
+				var array = a.onclick.toString().split("','");
+				var url = "/file/down/" + array[3] + array[4].split("'")[0] + "/" + code.value + ".html";
+				a = link(url);
+
+				// show wait
+				var div = document.querySelector("#premium_div");
+				console.info("div:", div);
+				if (div)
+					div.style.display = "none";
+				div = document.querySelector("#wait_div");
+				console.info("wait div:", div);
+				var span = document.querySelector("#wait_span");
+				console.info("wait span:", span);
+				if (!(div && span))
+					return;
+				div.style.display = "";
+				interval = 29;
+				setInterval(function () {
+					// jump
+					span.textContent = --interval;
+					if (!interval)
+						a.click();
+				}, 1000)
+			});
 		}
 		break;
 }
