b6fea08d3ce1f1fc46710cb6cb55d48c5a94a1d4
billypon <billypon@gmail.com>
2016-12-21 16:18:52 +0800

    update prevent-submit: support fmdisk.com

--- prevent-submit.js
+++ prevent-submit.js
@@ -2,10 +2,11 @@
 // @name        提交截拦
 // @author      billypon
 // @description 阻止某些输入框按回车时自动提交表单，改为点击默认按钮
-// @version     1.1.2
+// @version     1.2.0
 // @namespace   http://www.canaansky.com/
 // @match       http://66yp.cc/down-*.html
 // @match       http://www.123wzwp.com/down-*.html
+// @match       http://www.fmdisk.com/down2-*.html
 // @run-at      document-idle
 // @grant       none
 // ==/UserScript==
