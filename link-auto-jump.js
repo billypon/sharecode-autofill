4c873a634d510bda297d67403e67aaa91f416279
billypon <billypon@gmail.com>
2017-02-16 16:11:49 +0800

    update link-auto-jump/prevent-submit: feemoo.com

--- link-auto-jump.js
+++ link-auto-jump.js
@@ -2,14 +2,14 @@
 // @name        链接自动跳转
 // @author      billypon
 // @description 访问分享链接时自动跳转至下载页面或验证页面
-// @version     1.6.4
+// @version     1.6.5
 // @namespace   http://www.canaansky.com/
 // @match       http://158pan.cn/file-*.html
 // @match       http://66yp.cc/file-*.html
 // @match       http://www.123wzwp.com/file-*.html
 // @match       http://www.wwp5.com/file-*.html
 // @match       http://pan.789xz.com/file-*.html
-// @match       http://www.feemoo.com/*.html
+// @match       https://www.feemoo.com/*.html
 // @match       http://www.fxpan.com/downhtml/*.html
 // @match       http://*.dfpan.com/*
 // @run-at      document-idle
