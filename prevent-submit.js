ee5c09f47a793e6373880aa8e6c6f49312298714
billypon <billypon@gmail.com>
2017-05-10 15:49:58 +0800

    update prevent-submit: remove 66yp.cc, update for feemoo.com

--- prevent-submit.js
+++ prevent-submit.js
@@ -2,18 +2,33 @@
 // @name        提交截拦
 // @author      billypon
 // @description 阻止某些输入框按回车时自动提交表单，改为点击默认按钮
-// @version     1.4.2
+// @version     1.4.3
 // @namespace   http://www.canaansky.com/
 // @match       http://www.123wzwp.com/down-*.html
-// @match       http://66yp.cc/down-*.html
-// @match       https://www.feemoo.com/down2-*.html
+// @match       https://www.feemoo.com/fmdown.php?*
 // @match       http://www.sju.wang/down-*.html
 // @match       http://www.wwp5.com/down-*.html
 // @run-at      document-idle
 // @grant       none
 // ==/UserScript==
 
-var input = document.querySelector("#code"), button = document.querySelector("#s1");
+var domain = location.hostname.match(/\w+\.\w+$/)[0], path=location.pathname;
+console.debug("domain", domain);
+
+var input, button;
+
+switch (domain) {
+	case "feemoo.com":
+		input = "#very_btn";
+		button = "#vecysmt";
+		break;
+	default
+		input = "#code";
+		button = "#s1";
+}
+
+input = document.querySelector("#code");
+button = document.querySelector("#s1");
 console.debug("input", input);
 console.debug("button", button);
 if (!(input && button))
