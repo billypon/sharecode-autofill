021578bc9f93b1545382deab411e1d492f49c9ad
billypon <billypon@gmail.com>
2017-05-10 16:17:32 +0800

    update prevent-submit: feemoo.com

--- prevent-submit.js
+++ prevent-submit.js
@@ -2,10 +2,10 @@
 // @name        提交截拦
 // @author      billypon
 // @description 阻止某些输入框按回车时自动提交表单，改为点击默认按钮
-// @version     1.4.3
+// @version     1.4.4
 // @namespace   http://www.canaansky.com/
 // @match       http://www.123wzwp.com/down-*.html
-// @match       https://www.feemoo.com/fmdown.php?*
+// @match       http://www.feemoo.com/fmdown.php?*
 // @match       http://www.sju.wang/down-*.html
 // @match       http://www.wwp5.com/down-*.html
 // @run-at      document-idle
