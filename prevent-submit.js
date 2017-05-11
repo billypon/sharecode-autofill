c691994472da8293311313ec566f8421f6309fbb
billypon <billypon@gmail.com>
2017-05-11 10:31:28 +0800

    update prevent-submit

--- prevent-submit.js
+++ prevent-submit.js
@@ -2,7 +2,7 @@
 // @name        提交截拦
 // @author      billypon
 // @description 阻止某些输入框按回车时自动提交表单，改为点击默认按钮
-// @version     1.5.1
+// @version     1.6.0
 // @namespace   http://www.canaansky.com/
 // @match       http://www.123wzwp.com/down-*.html
 // @match       http://www.678pan.com/down-*.html
@@ -16,28 +16,41 @@
 var domain = location.hostname.match(/\w+\.\w+$/)[0], path=location.pathname;
 console.debug("domain", domain);
 
-var input, button;
+function prevent(input, button, element) {
+	if (!element)
+		element = document;
+	input = element.querySelector(input);
+	button = element.querySelector(button);
+	console.debug("input", input);
+	console.debug("button", button);
+	if (!(input && button))
+		return;
+	input.addEventListener("keydown", function (event) {
+		console.debug("keyCode", event.keyCode);
+		if (event.keyCode != 13)
+			return;
+		button.click();
+		event.preventDefault();
+	});
+}
+
+function listen(dialog, input, button) {
+	var handler = function (event) {
+		var target = event.target, list = target.classList;
+		if (!list || !list.contains(dialog))
+			return;
+		console.debug("dialog", target);
+		removeEventListener("DOMNodeInserted", handler);
+		prevent(input, button, target);
+	}
+	addEventListener("DOMNodeInserted", handler);
+}
 
 switch (domain) {
 	case "feemoo.com":
-		input = "#very_btn";
-		button = "#vecysmt";
+		listen("layui-layer", "#very_btn", "#vecysmt");
 		break;
 	default:
-		input = "#code";
-		button = "#s1";
+		prevent("#code", "#s1");
 }
 
-input = document.querySelector("#code");
-button = document.querySelector("#s1");
-console.debug("input", input);
-console.debug("button", button);
-if (!(input && button))
-	return;
-input.addEventListener("keydown", function (event) {
-	console.debug("keyCode", event.keyCode);
-	if (event.keyCode != 13)
-		return;
-	button.click();
-	event.preventDefault();
-});
