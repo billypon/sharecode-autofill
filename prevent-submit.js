d67f34bed3fdfe15d093e7ea6e83a417ded92848
billypon <billypon@gmail.com>
2017-07-07 11:54:38 +0800

    update prevent-submit: remove feemoo.com

--- prevent-submit.js
+++ prevent-submit.js
@@ -2,12 +2,11 @@
 // @name        提交截拦
 // @author      billypon
 // @description 阻止某些输入框按回车时自动提交表单，改为点击默认按钮
-// @version     1.9.0
+// @version     1.10.0
 // @namespace   http://www.canaansky.com/
 // @match       *://www.123wzwp.com/*
 // @match       *://www.678pan.com/*
 // @match       *://www.6pan.cc/*
-// @match       *://www.feemoo.com/*
 // @match       *://hiyp.cc/*
 // @match       *://www.sju.wang/*
 // @match       *://www.wwp5.com/*
@@ -57,11 +56,6 @@ function startsWith(search, string) {
 }
 
 switch (domain) {
-	case "feemoo.com":
-		if (startsWith("/fmdown.php")) {
-			listen("layui-layer", "#very_btn", "#vecysmt");
-		}
-		break;
 	default:
 		if (startsWith()) {
 			prevent("#code", "#s1");
