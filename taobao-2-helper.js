f95d4b0df02d58ea79667427d3a9ff2ab1e33c0b
billypon <billypon@gmail.com>
2018-05-17 22:43:34 +0800

    add taobao-2-helper

---
+++ taobao-2-helper.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name        闲鱼助手
+// @author      billypon
+// @description 闲鱼恢复搜索框、移除APP提示框、强制PC版页面
+// @version     1.0.0
+// @namespace   http://www.canaansky.com/
+// @match       *://2.taobao.com/*
+// @match       *://s.2.taobao.com/*
+// @match       *://g.alicdn.com/*
+// @run-at      document-idle
+// ==/UserScript==
+
+function restoreSearchBox() {
+	var header = document.querySelector("#J_IdleHeader");
+	var html = header.innerHTML;
+	html = html.replace(/<!--([\w\W\r\n]*?)-->/g, "$1");
+	html = html.replace('<input type="hidden"', '<input type="hidden" name="ist" value="0" /><input type="hidden"');
+	header.innerHTML = html;
+}
+
+function removeAppHints(dialog, input, button) {
+	document.querySelectorAll(".mau-guide, .download-layer").forEach(function (x) {
+		x.remove();
+	});
+}
+
+function redirectPage() {
+	var match = /itemid=(\d+)/.exec(location.search);
+	if (match) {
+		location = location.protocol + "//2.taobao.com/item.htm?id=" + match[1];
+	}
+}
+
+switch (location.hostname) {
+	case "g.alicdn.com":
+		redirectPage();
+		break;
+	default:
+		restoreSearchBox();
+		removeAppHints();
+		break;
+}
