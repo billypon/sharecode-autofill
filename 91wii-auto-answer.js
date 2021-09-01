ecfcfd69fab282bc15f88f6d2e941e2b406c2038
billypon <billypon@gmail.com>
2021-09-01 19:34:10 +0800

    add 91wii-auto-answer

---
+++ 91wii-auto-answer.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name        91wii自动回答
+// @description 自动回答运算
+// @version     1.0.0
+// @author      billypon
+// @namespace   http://www.canaansky.com/
+// @match       https://www.91wii.com/*
+// @run-at      document-idle
+// ==/UserScript==
+
+const submit = document.querySelector('[name="secqsubmit"]')
+if (submit) {
+	const container = submit.parentElement
+	const answer = container.querySelector('[name="answer"]')
+	const question = container.querySelector('b')
+	if (answer && question) {
+		answer.value = eval(`(${ question.textContent.replace(' = ?', '') })`)
+		submit.click()
+	}
+}
